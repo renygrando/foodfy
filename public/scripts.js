1f0bb2ab0da6454ec8217887d9df0880652e4203
renygrando <renygrando@gmail.com>
2020-05-07 21:29:03 -0300

    add nunjucks

---
+++ public/scripts.js
@@ -0,0 +1,23 @@
+const modalOverlay = document.querySelector('.modal_overlay');
+const cards = document.querySelectorAll('.quick_recipes');
+
+//mostra e esconde o modal overlay com o conteúdo
+
+for (let card of cards) {
+    card.addEventListener('click', function(){
+
+        const imgId = card.getAttribute('id');
+        const recipeTitle = card.querySelector('.recipes_title').textContent;
+        const recipeAuthor = card.querySelector('.recipes_author').textContent;
+        
+        modalOverlay.classList.add('active')
+
+        modalOverlay.querySelector(".modal_image_recipes").src = `/assets/${imgId}.png`
+        modalOverlay.querySelector(".modal_recipes_title").textContent = recipeTitle
+        modalOverlay.querySelector(".modal_recipes_author").textContent = recipeAuthor
+    })
+}
+
+document.querySelector('.close_modal').addEventListener('click', function (){
+    modalOverlay.classList.remove('active')
+})
