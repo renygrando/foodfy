21718b3e4fd76c1e774ce2807e8347a61ca23834
renygrando <renygrando@gmail.com>
2020-05-08 00:11:44 -0300

    incrementando servidor

--- server.js
+++ server.js
@@ -4,24 +4,30 @@ const nunjucks = require('nunjucks')
 
 //Chamando a função para utilizar no projeto
 const server = express()
+const recipes = require('./data')
 
 //Setando a view engine (motor de view) que vou utilizar, qual arquivo vou renderizar
-server.set('view engine', 'html')
+server.set('view engine', 'njk')
 
 //informando onde o servidor vai pegar os arquivos estáticos como CSS e Script
 server.use(express.static('public'))
+server.use(express.static('assets'))
 
 //Configurando o caminho das paginas que vou renderizar e qual opção, nesse caso estou usando o express passando a variável server.
 nunjucks.configure('views', {
     express: server
 })
-
-//Renderizando a página index na rota principal
+//Renderizando a página index na rota principal e outras paginas do site
 server.get('/', function(req, res) {
-    return res.render('index')
+    return res.render('index', {items: recipes})
 })
-
-//Mostrando que o servidor está funcionando
+server.get('/recipes', function(req, res) {
+    return res.render('recipes', {items: recipes})
+})
+server.get('/about', function(req, res) {
+    return res.render('about')
+})
+//Mostrando que o servidor está rodando
 server.listen(5000, function() {
     console.log ('server is running')
 })
