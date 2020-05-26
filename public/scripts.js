6ba13187ef2881c2ada79c089fcae8b98865f126
Reny Grando <renygrando@gmail.com>
2020-05-25 21:49:06 -0300

    Novas classes e script

--- public/scripts.js
+++ public/scripts.js
@@ -21,3 +21,47 @@
 // document.querySelector('.close_modal').addEventListener('click', function (){
 //     modalOverlay.classList.remove('active')
 // })
+
+const cards = document.querySelectorAll('.quick_recipes');
+
+for (let card of cards) {
+    card.addEventListener('click', function(){
+        const recipeId = card.getAttribute("id");
+        window.location.href = `/recipe-detail/${recipeId}`
+    })
+}
+
+// const hide = document.querySelector('.hide-btn');
+// const visibility = true;
+
+// function showHide() {
+//     if (visibility) {
+//         document.getElementById('recipe_showhide').style.display = 'none'
+//         visibility = false;
+//     } else {
+//         document.getElementById('recipe_showhide').style.display = 'block'
+//         visibility = true;
+//     }
+// } 
+
+// function showHide(id) {
+//     if (document.getElementById(id).style.display == 'block'){
+//         document.getElementById(id).style.display = 'none'
+//     } else {
+//         document.getElementById(id).style.display = 'block'
+//     }
+// }
+// document.querySelector('.hide-btn').addEventListener('click', showHide('recipe_showhide'))
+
+const ingredient = document.querySelector('recipe_showhide');
+const showHide = document.querySelector('showhide_btn');
+
+showHide.addEventListener('click', function() {
+    if (showHide.innerHTML == 'Esconder') {
+        ingredient.classList.add('active')
+        showHide.innerHTML = 'Mostrar'
+    } else {
+        ingredient.classList.remove('active')
+        showHide.innerHTML = 'Esconder'
+    }
+})
