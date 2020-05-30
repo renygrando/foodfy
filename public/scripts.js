abeaea09b08d81eaa5dda062751af1c60fb3477c
Reny Grando <renygrando@gmail.com>
2020-05-30 19:25:26 -0300

    criando script do botao mostra/esconde

--- public/scripts.js
+++ public/scripts.js
@@ -1,27 +1,4 @@
-// const modalOverlay = document.querySelector('.modal_overlay');
-// const cards = document.querySelectorAll('.quick_recipes');
-
-// //mostra e esconde o modal overlay com o conteúdo
-
-// for (let card of cards) {
-//     card.addEventListener('click', function(){
-
-//         const imgId = card.getAttribute('id');
-//         const recipeTitle = card.querySelector('.recipes_title').textContent;
-//         const recipeAuthor = card.querySelector('.recipes_author').textContent;
-        
-//         modalOverlay.classList.add('active')
-
-//         modalOverlay.querySelector(".modal_image_recipes").src = `${imgId}.png`
-//         modalOverlay.querySelector(".modal_recipes_title").textContent = recipeTitle
-//         modalOverlay.querySelector(".modal_recipes_author").textContent = recipeAuthor
-//     })
-// }
-
-// document.querySelector('.close_modal').addEventListener('click', function (){
-//     modalOverlay.classList.remove('active')
-// })
-
+//CRIANDO ROTAS PARA CADA RECEITA
 const cards = document.querySelectorAll('.quick_recipes');
 
 for (let card of cards) {
@@ -31,37 +8,18 @@ for (let card of cards) {
     })
 }
 
-// const hide = document.querySelector('.hide-btn');
-// const visibility = true;
+// CRIANDO BOTÃO MOSTRA-ESCONDE NA PÁGINA DE RECEITAS
+const ingredient = document.querySelectorAll('.recipe_showhide');
+const showHide= document.querySelectorAll('.showhide_btn');
 
-// function showHide() {
-//     if (visibility) {
-//         document.getElementById('recipe_showhide').style.display = 'none'
-//         visibility = false;
-//     } else {
-//         document.getElementById('recipe_showhide').style.display = 'block'
-//         visibility = true;
-//     }
-// } 
-
-// function showHide(id) {
-//     if (document.getElementById(id).style.display == 'block'){
-//         document.getElementById(id).style.display = 'none'
-//     } else {
-//         document.getElementById(id).style.display = 'block'
-//     }
-// }
-// document.querySelector('.hide-btn').addEventListener('click', showHide('recipe_showhide'))
-
-const ingredient = document.querySelector('.recipe_showhide');
-const showHide = document.querySelector('.showhide_btn');
-
-showHide.addEventListener('click', function() {
-    if (showHide.innerHTML == 'Esconder') {
-        ingredient.classList.add('active')
-        showHide.innerHTML = 'Mostrar'
-    } else {
-        ingredient.classList.remove('active')
-        showHide.innerHTML = 'Esconder'
-    }
-})
+for(let i = 0; i < showHide.length; i++) {
+    showHide[i].addEventListener('click', function() {
+        if (showHide[i].innerHTML == 'Esconder') {
+            ingredient[i].classList.add('active')
+            showHide[i].innerHTML = 'Mostrar'
+        } else {
+            ingredient[i].classList.remove('active')
+            showHide[i].innerHTML = 'Esconder'
+        }
+    })
+}
