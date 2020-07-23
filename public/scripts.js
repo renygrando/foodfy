87fe8a63a7544388692210209ffb4760217fb012
Reny Grando <renygrando@gmail.com>
2020-07-22 22:44:34 -0300

    trocando veja mais por abrir

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,8 @@
 //CRIANDO ROTAS PARA CADA RECEITA
-const cards = document.querySelectorAll('.quick_recipes');
+const cards = document.querySelectorAll('.recipes');
 
 for (let card of cards) {
-    card.addEventListener('click', function(){
+    card.addEventListener('click', function () {
         const recipeId = card.getAttribute("id");
         window.location.href = `/recipe-detail/${recipeId}`
     })
@@ -10,16 +10,16 @@ for (let card of cards) {
 
 // CRIANDO BOTÃO MOSTRA-ESCONDE NA PÁGINA DE RECEITAS
 const ingredient = document.querySelectorAll('.recipe_showhide');
-const showHide= document.querySelectorAll('.showhide_btn');
+const showHide = document.querySelectorAll('.showhide_btn');
 
-for(let i = 0; i < showHide.length; i++) {
-    showHide[i].addEventListener('click', function() {
-        if (showHide[i].innerHTML == 'Esconder') {
+for (let i = 0; i < showHide.length; i++) {
+    showHide[i].addEventListener('click', function () {
+        if (showHide[i].innerHTML == 'ABRIR') {
             ingredient[i].classList.add('active')
-            showHide[i].innerHTML = 'Mostrar'
+            showHide[i].innerHTML = 'FECHAR'
         } else {
             ingredient[i].classList.remove('active')
-            showHide[i].innerHTML = 'Esconder'
+            showHide[i].innerHTML = 'ABRIR'
         }
     })
 }
