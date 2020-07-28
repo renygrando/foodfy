f9a93f8483f706a4e0de986f8622cb07251610e1
Reny Grando <renygrando@gmail.com>
2020-07-27 23:18:53 -0300

    separando rotas por exports

---
+++ controllers/recipes.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const data = require('../data.json')
+
+exports.about = (req, res) => {
+    return res.render('about')
+}
+//pagina do quadro de receitas e detalhe de receitas
+exports.recipes = (req, res) => {
+    return res.render('recipes', {items: data.recipes})
+}
+
+exports.show = (req, res) => {
+    const id = req.params.id;
+    const recipe = data.recipes.find((recipe) => {
+        return recipe.id == id
+    })
+        
+    if (!recipe){
+        return res.send('Receita não encontrada')
+    }
+    return res.render('recipe-detail', {item: recipe})
+
+}
+//pagina do admin de receitas
+exports.index = (req, res) => {
+    return res.render("admin/recipes/index", {items: data.recipes})
+}
+
